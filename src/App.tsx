59ce2de057cef229c62e9ecf6c68076399aaa494
doarmi <dae04011@gmail.com>
2026-09-02 11:11:32 +0900

    Build portfolio landing page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import About from './sections/About'
-import Boot from './sections/Boot'
 import Contact from './sections/Contact'
 import KeyboardHero from './sections/KeyboardHero'
-import KeyboardOS from './sections/KeyboardOS'
 import Projects from './sections/Projects'
 import Skills from './sections/Skills'
 
@@ -13,9 +11,7 @@ gsap.registerPlugin(ScrollTrigger)
 export default function App() {
   return (
     <main>
-      <Boot />
       <KeyboardHero />
-      <KeyboardOS />
       <Projects />
       <About />
       <Skills />
